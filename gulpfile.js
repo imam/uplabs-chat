af553cc3b1d1bc7d3481eb9e0edc819249758b0b
Imam Assidiqqi <assidiqqi.imam@gmail.com>
2017-12-15 16:45:56 +0700

    Some update, and chat room

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp')
 var stylus = require('gulp-stylus')
+var poststylus = require('poststylus')
+var autoprefixer = require('autoprefixer')
+var lost = require('lost')
 
 gulp.task('style', function(){
 	return gulp.src('./style.styl')
-		.pipe(stylus())
+		.pipe(stylus({
+			use: [
+				poststylus([autoprefixer, lost])
+			]
+		}))
 		.pipe(gulp.dest('./dist'))
 })
 
